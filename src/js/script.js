74aa6356ae6ffd3d6ee84bd38693b089e48f770d
Konrad Gruzlo <konradgruzlo@Konrads-MacBook-Air.local>
2022-09-14 00:33:36 +0200

    add function ingredients visible

--- src/js/script.js
+++ src/js/script.js
@@ -80,6 +80,7 @@
       thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
       thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
       thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+      thisProduct.imageWrappe = thisProduct.element.querySelector(select.menuProduct.imageWrapper);
     }
 
     initAccordion() {
@@ -126,25 +127,36 @@
       // set price to default price
       let price = thisProduct.data.price;
 
+      const imgEl = (paramName, optionName) => thisProduct.imageWrappe.querySelector(`.${paramName}-${optionName}`);
+      
       // for every category (param)...
       for (let paramId in thisProduct.data.params) {
         // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
         const param = thisProduct.data.params[paramId];
         // console.log(paramId, param);
 
+        
         // for every option in this category
         for (let optionId in param.options) {
           // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
           const option = param.options[optionId];
           // console.log(optionId, option);
 
-          if(formData[paramId].includes(optionId) && !option.default) {
-              price += option.price;
+
+          if (formData[paramId].includes(optionId) && !option.default) {
+            price += option.price;
           }
-          if(!formData[paramId].includes(optionId) && option.default) {
-              price -= option.price;
+          if (!formData[paramId].includes(optionId) && option.default) {
+            price -= option.price;
           }
 
+          if(imgEl(paramId,optionId)){
+            if(formData[paramId].includes(optionId)) {
+              imgEl(paramId,optionId).classList.add(classNames.menuProduct.imageVisible)
+            } else {
+              imgEl(paramId,optionId).classList.remove(classNames.menuProduct.imageVisible)
+            }
+          }
         }
       }
 
