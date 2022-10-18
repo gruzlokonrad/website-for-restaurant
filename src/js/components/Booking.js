b0dc26608cb0528a0eecd6c570e83650bcceb557
Konrad Gruzlo <konradgruzlo@Konrads-MacBook-Air.local>
2022-10-18 22:14:47 +0200

    add DatePicker and HourPicker components

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,5 +1,7 @@
 import { select, templates } from '../settings.js';
 import AmountWidget from './AmountWidget.js';
+import DatePicker from './DatePicker.js';
+import HourPicker from './HourPicker.js';
 
 class Booking {
   constructor(widgetBooking) {
@@ -20,6 +22,8 @@ class Booking {
     thisWdigetBooking.dom.wrapper.innerHTML = generatedHTML;
     thisWdigetBooking.dom.peopleAmount = document.querySelector(select.booking.peopleAmount);
     thisWdigetBooking.dom.hoursAmount = document.querySelector(select.booking.hoursAmount);
+    thisWdigetBooking.dom.datePicker = document.querySelector(select.widgets.datePicker.wrapper);
+    thisWdigetBooking.dom.hourPicker = document.querySelector(select.widgets.hourPicker.wrapper);
   }
 
   initWidgets() {
@@ -34,6 +38,9 @@ class Booking {
     thisWdigetBooking.dom.hoursAmount.addEventListener('click', function (event) {
       event.preventDefault();
     });
+
+    new DatePicker(thisWdigetBooking.dom.datePicker);
+    new HourPicker(thisWdigetBooking.dom.hourPicker);
   }
 }
 
