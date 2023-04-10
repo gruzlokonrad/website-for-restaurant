1a614fea2999090c6ed5e86b8779cc5e00b93567
Konrad Gruzlo <gruzlokonrad@gmail.com>
2023-04-10 12:18:48 +0200

    update server for replit

--- server.mjs
+++ server.mjs
@@ -1,44 +1,47 @@
-import express from 'express';
-import cors from 'cors';
+import path from 'path';
+import jsonServer from 'json-server';
 
-const PORT = 3003;
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded());
-
-
-
-app.get('/', (req, res) => {
-  res.send('hello word');
+const server = jsonServer.create();
+const router = jsonServer.router(path.join('dist', 'db', 'app.json'));
+const middlewares = jsonServer.defaults({
+  static: 'dist',
+  noCors: true
 });
+const port = process.env.PORT || 3131;
 
-const products = [
-  {
-    name: 'book',
-    count: 20,
-  },
-  {
-    name: 'perosn',
-    count: 30,
-  }
-];
+server.use(middlewares);
+server.use(router);
 
-app.get('/products', (req, res) => {
-  res.json(products);
-});
+server.listen(port);
 
-app.post('/products', (req, res) => {
-  products.push(req.body);
-  res.send('Success!');
-});
+export default server;
 
+// import express from 'express';
+// import cors from 'cors';
 
+// const PORT = 3003;
+// const app = express();
+// const products = [
+//   { name: 'book', count: 20 },
+//   { name: 'perosn', count: 30, }
+// ];
 
+// app.use(cors());
+// app.use(express.json());
+// app.use(express.urlencoded());
 
+// app.get('/', (req, res) => {
+//   res.send('hello word');
+// });
+// app.get('/products', (req, res) => {
+//   res.json(products);
+// });
 
-app.listen(PORT, () => {
-  console.log('Listening on port PORT');
-});
+// app.post('/products', (req, res) => {
+//   products.push(req.body);
+//   res.send('Success!');
+// });
+
+// app.listen(PORT, () => {
+//   console.log('Listening on port PORT');
+// });
